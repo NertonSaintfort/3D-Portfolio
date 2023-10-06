3e985ef2ff01451ae7fcfd5d28fbd4ba5429f5c3
NertonSaintfort <SaintfortNerton@gmail.com>
2023-10-06 00:28:54 -0400

    second commit

--- 3D-Portfolio/src/App.jsx
+++ 3D-Portfolio/src/App.jsx
@@ -1,13 +1,31 @@
+import Hero from "./components/Hero";
+import Contact from "./components/Contact";
+import Who from "./components/Who";
+import Works from "./components/Works";
+import styled from "styled-components";
 
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-type: y mandatory;
+  scroll-behavior: smooth;
+  overflow-y: auto;
+  scrollbar-width: none;
+  color: white;
+  background: url("../public/img/bg.jpeg");
+  &::-webkit-scrollbar{
+    display: none;
+  }
+`;
 
 function App() {
- 
-
   return (
-    <div>
-    Hello World
-    </div>
-  )
+    <Container>
+      <Hero />
+      <Who />
+      <Works />
+      <Contact />
+    </Container>
+  );
 }
 
-export default App
+export default App;
---
+++ 3D-Portfolio/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const Section = styled.div`
+
+`
+
+const Navbar = () => {
+  return (
+    <div>
+      Navbar
+    </div>
+  );
+}
+
+export default Navbar;
