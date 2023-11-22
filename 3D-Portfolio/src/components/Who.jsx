295576acd3f92eb2909c22e1531ffde0718819c7
NertonSaintfort <SaintfortNerton@gmail.com>
2023-11-22 13:15:47 -0500

    final commit

--- 3D-Portfolio/src/components/Who.jsx
+++ 3D-Portfolio/src/components/Who.jsx
@@ -1,78 +1,88 @@
-import React, {Suspense} from 'react'
-import styled from "styled-components"
-import Cube from "./Cube"
+import React, { Suspense } from "react";
+import styled from "styled-components";
+import Cube from "./Cube";
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-
 
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
   display: flex;
   justify-content: center;
-`
+`;
 const Container = styled.div`
-height: 100vh;
-scroll-snap-align: center;
-width: 1400px;
-display: flex;
-justify-content: space-between;
-`
+  height: 100vh;
+  scroll-snap-align: center;
+  width: 1400px;
+  display: flex;
+  justify-content: space-between;
+`;
 
 const Left = styled.div`
-flex: 1;
+  flex: 1;
 
+  @media only screen and (min-width: 768px) {
+    display: none;
+    width: 0;
+  }
 `;
 const Title = styled.h1`
-font-size: 74px;
+  font-size: 74px;
+
+  @media only screen and (min-width: 768px) {
+    font-size: 60px;
+  }
 `;
 
 const Right = styled.div`
-flex: 1;
-display: flex;
-flex-direction: column;
-justify-content: center;
-gap: 20px;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
+
+  @media only screen and (min-width: 768px) {
+    align-items: center;
+    text-align: center;
+  }
 `;
 
 const WhatWeDo = styled.div`
-display: flex;
-align-items: center;
-gap: 10px;
+  display: flex;
+  align-items: center;
+  gap: 10px;
 `;
 
 const Line = styled.img`
-height: 5px;
+  height: 5px;
 `;
 
 const Subtitle = styled.h2`
-color: #da4ea2;
+  color: #da4ea2;
 `;
 
 const Desc = styled.p`
-font-size: 24px;
-color: lightgray;
+  font-size: 24px;
+  color: lightgray;
 `;
 
 const Button = styled.button`
-background-color: #da4ea2;
-color: white;
-font-size: 500;
-width: 120px;
-padding: 10px;
-border: none;
-border-radius: 5px;
-cursor: pointer;
+  background-color: #da4ea2;
+  color: white;
+  font-size: 500;
+  width: 120px;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
 `;
-
-
 
 const Who = () => {
   return (
     <Section>
       <Container>
         <Left>
-        <Canvas camera={{ position: [5, 5, 5], fov: 25 }}>
+          <Canvas camera={{ position: [5, 5, 5], fov: 25 }}>
             <Suspense fallback={null}>
               <ambientLight intensity={0.5} />
               <directionalLight position={[3, 2, 1]} />
@@ -82,13 +92,14 @@ const Who = () => {
           </Canvas>
         </Left>
         <Right>
-        <Title>Think outside the square space</Title>
+          <Title>Think outside the square space</Title>
           <WhatWeDo>
             <Line src="./img/line.png" />
             <Subtitle>Who we Are</Subtitle>
           </WhatWeDo>
           <Desc>
-          a creative group of designers and developers with a passion for the arts.
+            a creative group of designers and developers with a passion for the
+            arts.
           </Desc>
           <Button>See our works</Button>
         </Right>
@@ -97,4 +108,4 @@ const Who = () => {
   );
 };
 
-export default Who
+export default Who;
